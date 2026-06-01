f3a1b5fbfc9e3901a80ebb79c7e2ab7c34cfbb32
CozyCrypto <fassdavid722@gmail.com>
2026-06-01 19:58:21 +0100

    feat(client): wrap app with tRPC + QueryClientProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { trpc, getTrpcClient } from "./lib/trpc";
+import { queryClient } from "./lib/queryClient";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+function Root() {
+  const [trpcClient] = useState(() => getTrpcClient());
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <Root />
+  </StrictMode>
+);
